7cccc6822ebefdc9a03f877921f24290a4c8c89f
Szymon Góral <matiqueue@gmail.com>
2025-04-07 18:32:32 +0200

    fix: remove disableTransitionOnChange prop from ThemeProvider for improved functionality

--- providers/providers.tsx
+++ providers/providers.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from '@/providers/theme-provider'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       {children}
     </ThemeProvider>
   )
