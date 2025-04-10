8426b31cb633d5ce308048f45b964184b9607440
Szymon Góral <matiqueue@gmail.com>
2025-04-11 00:10:40 +0200

    refactor: simplify getUserData function by removing error handling and logging

--- bot/functions/user-and-ip/user-data.ts
+++ bot/functions/user-and-ip/user-data.ts
@@ -1,26 +1,11 @@
 import axios from 'axios'
 
 export const getUserData = async (token: string) => {
-  try {
-    const response = await axios.get('https://discord.com/api/v10/users/@me', {
-      headers: {
-        Authorization: token,
-      },
-    })
+  const response = await axios.get('https://discord.com/api/v10/users/@me', {
+    headers: {
+      Authorization: token,
+    },
+  })
 
-    console.log('Dane użytkownika:', response.data)
-    return response.data
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (error: any) {
-    if (error.response) {
-      console.warn(
-        `Błąd odpowiedzi Discord API: ${error.response.status} ${error.response.statusText}`
-      )
-    } else {
-      console.error('Axios się wywalił:', error.message)
-    }
-    return null
-  }
+  return response.data
 }
-
-getUserData('')
