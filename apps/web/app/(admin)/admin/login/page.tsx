af1e1163fd0bc5c5e96e85b92d332b194e404f36
seba twr ekipa <sebastianpodgorski@o2.pl>
2025-04-23 18:20:52 +0200

    admin login

--- apps/web/app/(admin)/admin/login/page.tsx
+++ apps/web/app/(admin)/admin/login/page.tsx
@@ -1,3 +1,19 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+
 export default function AdminLoginPage() {
-  return <div>Stronka do logowania admina</div>
+  const searchParams = useSearchParams();
+  const error = searchParams.get("error");
+
+  return (
+  <div>
+    <div>Stronka do logowania admina</div>
+    <form action="/api/admin-login" method="post">
+      <h1> Podaj login: </h1><input type="text" name="login"/>
+      <h1> Podaj hasło: </h1><input type="password" name="password"/>
+      <button type="submit">Zaloguj się</button>
+    </form>
+    {error && <p style={{ color: "red" }}>Niepoprawny login lub hasło</p>}
+  </div>
+  )
 }
