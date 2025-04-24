5d106df57b15204c091858e2f9350c4103d6bc29
Szymon Góral <matiqueue@gmail.com>
2025-04-24 04:20:28 +0200

    Remove unused font styles from admin layout

--- apps/admin/app/layout.tsx
+++ apps/admin/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}>
+      <body className={`${fontSans.variable}`}>
         <Providers>
           <AuthComponent />
           {children}
